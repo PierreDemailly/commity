087039bbe3c8449bff3524f80969f9e21f403358
PIERRE DEMAILLY <pierre.demailly-isc.france@ibm.com>
2019-12-03 16:16:24 +0100

    feat commity v1

---
+++ bin/cli.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+// external libs
+const chalk   = require('chalk');
+const fs      = require('file-system');
+const program = require('commander');
+
+// internal libs
+const init    = require('../lib/init');
+const commity = require('../lib/commity');
+const utils    = require('../lib/utils');
+
+/**
+ * Define if there is commity.json in cwd.
+ * If user run "commity init"
+ * then this variable will be set falsy so commity will not run.
+ */
+let initialized = true;
+
+/**
+ * Check if current directory is a Git repository
+ */
+const isGitInit = fs.existsSync(process.cwd() + '/.git')
+if (!isGitInit) {
+  console.log(chalk.red('Current directory is not a Git repository.'));
+  process.exit();
+}
+
+/**
+ * CLI (sub)commands
+ *
+ * example of use: commity init
+ */
+program
+  .command('init')
+  .description('init your workspace to make it commity friendly')
+  .action(() => {
+    initialized = false;
+    init();
+  })
+
+/**
+ * CLI available options
+ *
+ * example of use: commity -p --addAll
+ */
+program
+  .allowUnknownOption()
+  .option('-p, --push', 'push changes to current remote branch after commiting')
+  .option('-a, --addAll', 'add all staged changes before commiting')
+
+/**
+ * Parse program arguments
+ */
+program.parse(process.argv);
+
+if (initialized) {
+  commity(program);
+}
+
+// TODO: commity --addAll --push => git add --all && commity && git push
+// TODO: commity -A -P           => git add --all && commity && git push
