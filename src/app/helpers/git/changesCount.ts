07bf3601c69b55db5f47a00140b2b526e97e35e9
PierreDemailly <pierredemailly.pro@gmail.com>
2021-03-24 20:28:52 +0100

    refactor #14: remove unused try-catch

--- src/app/helpers/git/changesCount.ts
+++ src/app/helpers/git/changesCount.ts
@@ -1,17 +1,13 @@
 import { exec } from 'child_process';
 
 export const gitChangesCount = (): Promise<number> => {
-  try {
-    return new Promise((resolve, reject) => {
-      exec('git status -s', (err, stdout, stderr) => {
-        if (err) {
-          resolve(0);
-        } else {
-          resolve(stdout.split('\n').length - 1);
-        }
-      });
+  return new Promise((resolve, reject) => {
+    exec('git status -s', (err, stdout, stderr) => {
+      if (err || stderr) {
+        resolve(0);
+      } else {
+        resolve(stdout.split('\n').length - 1);
+      }
     });
-  } catch (error) {
-    throw new Error(error);
-  }
+  });
 }
