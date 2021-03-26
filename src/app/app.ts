f9240e82e50b8f7954d851c40d61a933b2d1fd2d
PierreDemailly <pierredemailly.pro@gmail.com>
2021-03-26 23:24:23 +0100

    spec #12: 100% coverage app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -24,57 +24,57 @@ export class App {
 
     await this.isCommityFriendly();
     Commity.run(nezparser as Inezparser);
-    }
+  }
 
-    async isCommityFriendly(): Promise < void> {
-      try {
-        this.conf = await import(join(process.cwd(), 'commity.json'));
-      } catch(error) {
-        tricolors.redLog('Commity is not initialized. Please run "commity init" to init your workspace.');
-        process.exit();
-      }
-    }
-
-    async isGitInitialized(): Promise < void> {
-      try {
-        const path = join(process.cwd(), '/.git');
-        await fs.promises.access(path, fs.constants.F_OK);
-        return;
-      } catch(error) {
-        tricolors.redLog('Current directory is not a Git repository.');
-        process.exit();
-      }
-    }
-
-    setupParser(): void {
-      nezparser.setup({
-        usage: 'commity <command> <options>',
-        options: [
-          {
-            name: '--push',
-            alias: '-p',
-            description: 'push changes to current remote branch after commiting',
-          },
-          {
-            name: '--addAll',
-            alias: '-a',
-            description: 'add all staged changes before commiting',
-          },
-        ],
-        commands: [
-          {
-            name: 'init',
-            description: 'inititialize Commity',
-            options: [
-              {
-                name: '--overwrite',
-                alias: '-o',
-                description: 'overwrite existing commity.json (if exists)',
-              },
-            ],
-          },
-        ],
-      });
-      nezparser.parse();
+  async isCommityFriendly(): Promise<void> {
+    try {
+      this.conf = await import(join(process.cwd(), 'commity.json'));
+    } catch (error) {
+      tricolors.redLog('Commity is not initialized. Please run "commity init" to init your workspace.');
+      process.exit();
     }
   }
+
+  async isGitInitialized(): Promise<void> {
+    try {
+      const path = join(process.cwd(), '/.git');
+      await fs.promises.access(path, fs.constants.F_OK);
+      return;
+    } catch (error) {
+      tricolors.redLog('Current directory is not a Git repository.');
+      process.exit();
+    }
+  }
+
+  setupParser(): void {
+    nezparser.setup({
+      usage: 'commity <command> <options>',
+      options: [
+        {
+          name: '--push',
+          alias: '-p',
+          description: 'push changes to current remote branch after commiting',
+        },
+        {
+          name: '--addAll',
+          alias: '-a',
+          description: 'add all staged changes before commiting',
+        },
+      ],
+      commands: [
+        {
+          name: 'init',
+          description: 'inititialize Commity',
+          options: [
+            {
+              name: '--overwrite',
+              alias: '-o',
+              description: 'overwrite existing commity.json (if exists)',
+            },
+          ],
+        },
+      ],
+    });
+    nezparser.parse();
+  }
+}
--- src/spec/app.spec.ts
+++ src/spec/app.spec.ts
@@ -1,18 +1,72 @@
-const { App } = require('./../app/app');
-const _chai = require('chai');
-_chai.should();
+jest.mock("fs", () => ({
+  promises: {
+    access: jest.fn().mockResolvedValue(Promise.resolve()),
+  },
+  constants: {
+    F_OK: 0,
+  }
+}));
+
+import path from 'path';
+import { join } from 'path';
+import fs from 'fs';
+import tricolors from 'tricolors';
+
+import { Init } from './../app/commands/init';
+import { Commity } from './../app/commity';
+import { App } from './../app/app';
 
 describe('App', () => {
-  const app = new App();
+  let app = new App();
 
   it('should be defined', () => { 
-      app.should.be.not.undefined;
+      expect(app).toBeTruthy();
   });
 
   it('should initialize', async () => {
-    const spy = jest.spyOn(app, 'isGitInitialized').mockReturnValue(() => Promise.resolve());
+    const spy = jest.spyOn(app, 'isGitInitialized').mockReturnValue(Promise.resolve());
+    const spyB = jest.spyOn(Commity, 'run').mockReturnValue(Promise.resolve());
+    await app.initialize();
+    expect(spy).toHaveBeenCalled();
+    expect(spyB).toHaveBeenCalled();
+    spy.mockRestore();
+    spyB.mockRestore();
+  });
+
+  it('should run Init.initialize()', async () => {
+    process.argv = ['1', '1', 'init'];
+    jest.spyOn(app, 'isGitInitialized').mockReturnValue(Promise.resolve());
+    const spy = jest.spyOn(Init, 'initialize').mockReturnValue(Promise.resolve());
     await app.initialize();
     expect(spy).toHaveBeenCalled();
     spy.mockRestore();
+  });
+
+  it('should not be commity friendly', async () => {
+    app = new App();
+    process.argv = ['1', '1', '1'];
+    spyOn(process, 'exit').and.callFake(() => {});
+    spyOn(path, 'join').and.callFake(() => Promise.reject());
+    jest.spyOn(tricolors, 'redLog').mockReturnValue(void null);
+    await app.isCommityFriendly();
+    expect(process.exit).toHaveBeenCalled();
+    expect(tricolors.redLog).toHaveBeenCalledWith('Commity is not initialized. Please run "commity init" to init your workspace.');
+  });
+
+  it('repo should be git initialized', async () => {
+    app = new App();
+    process.argv = ['1', '1', '1'];
+    await app.isGitInitialized();
+    expect(fs.promises.access).toHaveBeenCalledWith(`${join(process.cwd(), '/.git')}`, fs.constants.F_OK);
+  });
+
+  it('repo should not be git initialized', async () => {
+    app = new App();
+    jest.spyOn(fs.promises, 'access').mockRejectedValue('err');
+    jest.spyOn(tricolors, 'redLog').mockReturnValue(void null);
+    spyOn(process, 'exit').and.callFake(() => {});
+    await app.isGitInitialized();
+    expect(process.exit).toHaveBeenCalled();
+    expect(tricolors.redLog).toHaveBeenCalledWith('Current directory is not a Git repository.');
   });
 })
