517b99dbc366157523eb07713f1a0994ca4edadd
PierreDemailly <pierredemailly.pro@gmail.com>
2021-03-27 12:30:39 +0100

    [0;32mCommited 2 files. [0m[1mspec #12: add missing mocks[0m

--- src/spec/commity.spec.ts
+++ src/spec/commity.spec.ts
@@ -3,37 +3,35 @@ import { Conf } from '../app/app';
 import { fields } from '../app/helpers/core/fields';
 import { Commity } from './../app/commity';
 
-jest.mock('../app/helpers/core/fields', () => ({
-  fields: jest.fn().mockResolvedValue(Promise.resolve()),
-}));
+jest.mock('../app/helpers/core/fields');
 jest.mock("../app/helpers/git/changesCount", () => ({
   gitChangesCount: jest.fn().mockResolvedValue(Promise.resolve(748)),
 }));
 describe('Commity', () => {
   const commity = new Commity({ hasOption: (opt: string, alias: string) => true } as Inezparser, {} as Conf);
-  commity.result = {
-    render: 'render',
-    values: [{
-      foo: 'bar',
-    }],
-  } as any;
 
   it('should be defined', () => {
     expect(commity).toBeTruthy();
   });
 
-  it('should have 0 changes', async () => {
+  it('should have 748 changes', async () => {
     commity.result = {
       render: 'render',
       values: [{
         foo: 'bar',
       }],
     } as any;
+    commity.conf = {
+      render: 'foo bar',
+    } as any;
     spyOn(commity.nezparser, 'hasOption').and.callFake(() => Promise.resolve()),
     spyOn(commity, 'handleAddAllOption').and.callFake(() => Promise.resolve());
+    spyOn(commity, 'getFields').and.callFake(() => Promise.resolve());
     spyOn(commity, 'checkStagedCount').and.callFake(() => Promise.resolve());
     spyOn(commity, 'commit').and.callFake(() => Promise.resolve());
     spyOn(commity, 'handlePushOption').and.callFake(() => Promise.resolve());
+    spyOn(process, 'exit').and.callFake(() => {});
+    spyOn(console, 'log').and.callFake(() => {});
     await commity.run();
     expect(commity.changesCount).toEqual(748);
   });
